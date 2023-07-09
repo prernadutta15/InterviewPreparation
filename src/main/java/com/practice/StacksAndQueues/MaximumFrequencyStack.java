0155f006cb3bb4771e9a8343e691d7eee51aa9f7
PrernaDutta <prerna.dutta@blr.cordis>
2023-07-09 19:54:09 +0530

    Updated: Min Stack

---
+++ src/main/java/com/practice/StacksAndQueues/MaximumFrequencyStack.java
@@ -0,0 +1,60 @@
+package com.practice.StacksAndQueues;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * 
+ * @author prerna.dutta
+ *
+ */
+
+/*
+ * https://leetcode.com/problems/maximum-frequency-stack/description/
+ */
+
+public class MaximumFrequencyStack {
+	Map<Integer,Stack<Integer>> map;
+    Map<Integer,Integer> freqMap;
+    int maxFreq=1;
+    public MaximumFrequencyStack() {
+        map = new HashMap<>();
+        freqMap = new HashMap<>();
+        maxFreq=0;
+    }
+    
+    public void push(int val) {
+        int freq = freqMap.getOrDefault(val,0)+1;
+        maxFreq = Math.max(maxFreq,freq);
+        freqMap.put(val, freq);
+        Stack<Integer> stack = map.get(freq);
+        if(stack == null)
+            stack = new Stack<>();
+        stack.push(val);
+        map.put(freq,stack);
+    }
+    
+    public int pop() {
+       Stack<Integer> stack = map.get(maxFreq);
+       
+       int ele = stack.pop();
+       if(stack.isEmpty()){
+           map.remove(maxFreq);
+           maxFreq--;
+       }
+       else
+       map.put(maxFreq, stack);
+       int freq = freqMap.get(ele)-1;
+
+       if(freq==0)
+       {
+           freqMap.remove(ele);
+       }else
+            freqMap.put(ele,freq);
+       return ele;
+    }
+    public static void main(String[] args) {
+		System.out.println("ok");
+	}
+}
--- src/main/java/com/practice/StacksAndQueues/MaximumWindow.java
+++ src/main/java/com/practice/StacksAndQueues/MaximumWindow.java
@@ -1,6 +1,9 @@
-package com.practice.coding.linked.list;
+package com.practice.StacksAndQueues;
 
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 public class MaximumWindow 
 {
